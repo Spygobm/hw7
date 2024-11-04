7e68dddeec8b6f7ff694cd3e0aec174eb232035d
Spygobm <stuartpotter2011@icloud.com>
2024-11-04 18:05:26 +0000

    Add files via upload

---
+++ hw7/main.py
@@ -0,0 +1,23 @@
+pizza =("Rome","tuesday","2020")
+place,day,year = pizza
+
+
+n_tuples = ("pizza",[200,300,400],(True,323543,"eating"))
+
+print(n_tuples[1])
+print(n_tuples[0])
+print(n_tuples[1][1])
+print(n_tuples[2][1])
+print(n_tuples[0][4])
+
+eating = "pizza",True,1
+print(eating)
+
+my_tuples = ("w","o","r","d","i","n","g",)
+
+print(my_tuples[2:6])
+print(my_tuples[4:])
+print(my_tuples[:5])
+
+#my_tuples[2]="x"
+#This has given an error becouse tuples cannot be changed using a seperate code 
